4f59a968d252b73a2e4096b771e41bb7993c3c81
yuan qihang <qihang.yuan@thoughtworks.com>
2020-11-17 23:36:47 +0800

    p2s1

---
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/LevelControllerTest.java
@@ -0,0 +1,26 @@
+package com.thoughtworks.capability.gtb.demospringconfig;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+public class LevelControllerTest {
+
+    @Autowired
+    private LevelController levelController;
+
+
+    @Test
+    void get_level_of_basic() {
+        assertEquals("advanced",levelController.getLevel());
+    }
+
+}
